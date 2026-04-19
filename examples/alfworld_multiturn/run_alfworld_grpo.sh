bd712bcfa241aff05aed92ede985d86c7b6f73d9
Roxy <2845331774@qq.com>
2026-04-19 19:16:25 +0800

    On branch main Changes to be committed: 	modified:   examples/alfworld_multiturn/configs/alfworld_grpo.yaml 	modified:   examples/alfworld_multiturn/run_alfworld_grpo.sh

--- examples/alfworld_multiturn/run_alfworld_grpo.sh
+++ examples/alfworld_multiturn/run_alfworld_grpo.sh
@@ -46,7 +46,7 @@ python3 -m verl.trainer.main_ppo \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
     actor_rollout_ref.model.path="$MODEL_PATH" \
-    +actor_rollout_ref.model.override_config.attn_implementation=eager \
+    actor_rollout_ref.model.override_config.attn_implementation=eager \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=128 \
@@ -56,8 +56,12 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
     actor_rollout_ref.actor.entropy_coeff=0 \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.actor.fsdp_config.mixed_precision.param_dtype=fp16 \
+    actor_rollout_ref.actor.fsdp_config.mixed_precision.reduce_dtype=fp32 \
+    actor_rollout_ref.actor.fsdp_config.mixed_precision.buffer_dtype=fp32 \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+    actor_rollout_ref.rollout.dtype=float16 \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=8 \
     actor_rollout_ref.rollout.name="$ROLLOUT_NAME" \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
@@ -66,6 +70,9 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.mode=async \
     actor_rollout_ref.rollout.val_kwargs.temperature=0.4 \
     actor_rollout_ref.rollout.multi_turn.max_assistant_turns=50 \
+    actor_rollout_ref.ref.fsdp_config.mixed_precision.param_dtype=fp16 \
+    actor_rollout_ref.ref.fsdp_config.mixed_precision.reduce_dtype=fp32 \
+    actor_rollout_ref.ref.fsdp_config.mixed_precision.buffer_dtype=fp32 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=8 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.use_kl_in_reward=False \
